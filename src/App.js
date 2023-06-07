700bc0aaf7bcc2ceca81692b423e481eab6c3dba
phi_ironman <nguyentruongphi15032003@gmail.com>
2023-06-07 15:33:14 +0700

    radio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+// const gifts = [
+//   'CPU i9',
+//   'RAM 32GB RGB',
+//   'RGB Keyboard'
+// ]
+
+const courses = [
+  {
+    id: 1,
+    name: 'JavaScript'
+  },
+  {
+    id: 2,
+    name: 'PHP'
+  },
+  {
+    id: 3,
+    name: 'Java'
+  }
+]
 
 function App() {
+  const [checked, setChecked] = useState();
+
+  const handleSubmit = () => {
+    console.log({ id: checked });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: 20 }}>
+      {courses.map(course => (
+        <div ket={course.id}>
+          <input
+            type='radio'
+            checked={checked === course.id}
+            onChange={() => setChecked(course.id)}
+          />
+          {course.name}
+        </div>
+      ))}
+
+      <button onClick={handleSubmit}>Register</button>
     </div>
   );
 }
