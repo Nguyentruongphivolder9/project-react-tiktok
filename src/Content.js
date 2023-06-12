78404cd50fea211b0aff98035359acc3cdf9e89c
phi_ironman <nguyentruongphi15032003@gmail.com>
2023-06-12 18:06:53 +0700

    cleaup function file

--- src/Content.js
+++ src/Content.js
@@ -8,6 +8,8 @@ function Content() {
     const [type, setType] = useState('comments');
     const [showGoToTop, setShowGoToTop] = useState(false);
     const [clock, setClock] = useState(180);
+    const [avatar, setAvatar] = useState();
+
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/${type}`)
             .then(res => res.json())
@@ -37,14 +39,42 @@ function Content() {
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            setClock(prev => prev - 1);
-        }, 1000)
+            setClock(prev => {
+                if(prev === 0){
+                    prev = 180;
+                } else {
+                    prev = prev - 1;
+                }
+                return prev;
+            });
+        }, 1000);
 
         return () => clearInterval(intervalId);
     }, [])
 
+    const handleImageFile = (e) => {
+        const file = e.target.files[0];
+
+        file.preview = URL.createObjectURL(file);
+        
+        setAvatar(file);
+    }
+
+    useEffect(() => {
+        return () => {
+            avatar && URL.revokeObjectURL(avatar.preview);
+        }
+    }, [avatar]);
+
     return (
         <div>
+            <input 
+                type="file"
+                onChange={handleImageFile}
+            />
+            {avatar && (
+                <img src={avatar.preview} alt='' width="80%" />
+            )}
             <h1>{clock}</h1>
             {tabs.map(tab => (
                 <button
